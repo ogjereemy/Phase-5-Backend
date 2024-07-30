2802aefde6cd4f9afcdd2c7385c027e90a24c852
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-07-30 23:14:46 +0300

    adds users and goals views

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask_jwt_extended import jwt_required,current_user,get_jwt_identity,JWTMan
 # from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 # from flask_cors import CORS
-from models import db
+from models import *
 from datetime import timedelta
 from config import *
+
 
 
 app.config['SECRET_KEY'] = 'bf33b4e2a163a29294876531f6ba53ead9'
@@ -18,6 +19,117 @@ app_api = Api(app_bp)
 app.register_blueprint(app_bp)
 # app.register_blueprint(auth_bp)
 
+class Users(Resource):
+    def get(self):
+        users=User.query.all()
+        users_dict= [user.to_dict()for user in users]
+        response = make_response(jsonify(users_dict),200)
+        return response
+    
+    def delete(self):
+        user_id= request.get_json()['user_id']
+        user=User.query.filter_by(id=user_id).first()
+        db.session.delete(user)
+        db.session.commit()
+        response=make_response(jsonify({'msg':'User deleted successfully'}),200)
+        return response
+        
+
+goals_args=reqparse.RequestParser()
+goals_args.add_argument('title',type=str)
+goals_args.add_argument('description',type=str)
+goals_args.add_argument('target_date',type=date)
+goals_args.add_argument('achieved',type=bool)
+
+class Goals(Resource):
+    def post(self):
+        data=goals_args.parse_args()
+        user_id =get_jwt_identity()
+        new_goal=Goal(
+            user_id=user_id,
+            title=data.get('title'),
+            description=data.get('description'),
+            target_date= data.get('target_date'),
+            achieved=data.get('achieved')
+        )
+        db.session.add(new_goal)
+        db.session.commit()
+        new_goal_dict=new_goal.to_dict()
+        response = make_response(jsonify(new_goal_dict),201)
+        return response
+
+
+
+    def get(self):
+        goals=Goal.query.all()
+        goals_dict=[goal.to_dict()for goal in goals]
+        response=make_response(jsonify(goals_dict),200)
+        return response
+
+    def patch(self):
+        data=goals_args.parse_args()
+        description= data.get('description')
+        target_date= data.get('target_date')
+        goal_id=request.get_json()['goal_id']
+        goal=Goal.query.filter_by(id=goal_id)
+        if goal :
+            goal.description=description
+            goal.target_date=target_date
+            goal_dict=goal.to_dict()
+            response=make_response(jsonify(goal_dict),200)
+            return response
+        else : 
+            response=make_response(jsonify({'msg':'Goal not found'}),404)
+            return response
+
+    def delete(self):
+        # user_id=get_jwt_identity()
+        goal_id=request.get_json()['goal_id']
+        goal=Goal.query.filter_by(id=goal_id).first()
+        db.session.delete(goal)
+        db.session.commit()
+        response=make_response(jsonify({'msg':'Goal deleted successfully'}),200)
+        return response
+
+        
+
+class Coaches(Resource):
+    pass
+
+    
+
+
+class NutritionLogs(Resource):
+    pass
+
+class ProgressLogs(Resource):
+    pass
+
+class Exercises(Resource):
+    pass
+
+class Workouts(Resource):
+    pass
+
+class WorkoutPlans(Resource):
+    pass
+
+class UserProfile(Resource):
+    pass
+
+class CoachProfile(Resource):
+    pass
+
+app_api.add_resource(Goals,'/goals')
+app_api.add_resource(Workouts,'/workouts')
+app_api.add_resource(Exercises,'/exercises')
+app_api.add_resource(NutritionLogs,'/nutrition_logs')
+app_api.add_resource(ProgressLogs,'/progress_logs')  
+app_api.add_resource(WorkoutPlans,'/workout_plans')
+app_api.add_resource(Users,'/users') ##coaches
+app_api.add_resource(Coaches,'/coaches') #admin
+app_api.add_resource(UserProfile,'/profile') ##user
+app_api.add_resource(CoachProfile,'/coach_Profile')
 
 
 
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import jwt_required,current_user,get_jwt_identity,JWTMan
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-# from models import db
+
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
